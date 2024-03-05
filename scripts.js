a3a3d81a798a4540961b3339ddc6fab9404f58cc
twapor <totwagnerapor@gmail.com>
2024-03-05 16:53:49 +0200

    Created a basic version of calculator

--- scripts.js
+++ scripts.js
@@ -62,24 +62,27 @@ addition.addEventListener('click', () => {
 const calculation = document.querySelector('#btn-equals');
 calculation.addEventListener('click', () => {
     updateSecondaryInterface();
+    operate();
+    
+
 })
 
 
 
-function updateMainInterface(input){
+function updateMainInterface(input) {
     largeScreen.textContent += input;
 };
 
-function updateSecondaryInterface(){
+function updateSecondaryInterface() {
     smallScreen.textContent = largeScreen.textContent;
-    clearMainInterface();
+    
 }
 
-function clearMainInterface(){
+function clearMainInterface() { 
     largeScreen.textContent = "";
 }
 
-function clearSecondaryInterface(){
+function clearSecondaryInterface() {
     smallScreen.textContent = "";
 }
 
@@ -92,26 +95,117 @@ function deleteLastCharacter() {
     }
 }
 
+function createOperableArrays() {
+    let str = largeScreen.textContent;
+    let arrayStr = str.split(" ");
+    let arrayTempNum = arrayStr.map(str => {
+        return parseFloat(str);
+    });
+    let arrayIndex = findIndexOfNan(arrayTempNum);
+    const arrayNum = arrayTempNum.filter(function (value) {
+        return !Number.isNaN(value);
+    });
+    const arrayOperator = [];
+    for(let i = 0; i < arrayIndex.length; i++){
+        arrayOperator.push(arrayStr.at(arrayIndex.at(i)));
+    };
+    let object = {
+        numbers: arrayNum,
+        operators: arrayOperator
+    }
+    return object;
+}
+
+function findIndexOfNan(array) {
+    return array.map(function(el, i){
+        if(isNaN(el)) 
+          return i;
+        return false;
+    }).filter(function(el){
+        return el;
+    });
+}
+
+function operate() {
+    let object = createOperableArrays();
+    const arrayOperator = object.operators;
+    const arrayNum = object.numbers;
+    let j = 0;
+    let calcVal = 0;
+    for(let i=0; i < arrayNum.length;){
+        if(j < arrayOperator.length){
+            if(i < 1){
+                if(arrayOperator[j] == '+'){
+                    calcVal = add(arrayNum[i], arrayNum[i+1]);
+                    i +=2;
+                    j++;
+                }
+                else if (arrayOperator[j] == '-'){
+                    calcVal = subtract(arrayNum[i], arrayNum[i+1]);
+                    i +=2;
+                    j++;
+                }
+                else if (arrayOperator[j] == 'x'){
+                    calcVal = multiply(arrayNum[i], arrayNum[i+1]);
+                    i +=2;
+                    j++;
+                }
+                else if (arrayOperator[j] == '/'){
+                    if(arrayNum[i+1] === 0){
+                        largeScreen.textContent= 'Cannot divide with zero!'
+                        break;
+                    }
+                    else {
+                        calcVal = divide(arrayNum[i], arrayNum[i+1]);
+                        i +=2;
+                        j++; 
+                    }
+                }
+            }
+            else {
+                if(arrayOperator[j] == '+'){
+                    calcVal = add(calcVal, arrayNum[i]);
+                    i++;
+                    j++
+                }
+                else if (arrayOperator[j] == '-'){
+                    calcVal = subtract(calcVal, arrayNum[i]);
+                    i++;
+                    j++;
+                }
+                else if (arrayOperator[j] == 'x'){
+                    calcVal = multiply(calcVal, arrayNum[i]);
+                    i++;
+                    j++;
+                }
+                else if (arrayOperator[j] == '/'){
+                    if(arrayNum[i] === 0){
+                        largeScreen.textContent= 'Cannot divide with zero!'
+                        break;
+                    }
+                    else {
+                        calcVal = divide(calcVal, arrayNum[i]);
+                        i++;
+                        j++;
+                    }
+                }
+            }
+        }
+        else {
+            console.log('calculation finished');
+            i++;
+        }
+
+        largeScreen.textContent = calcVal;
+    }
+}
+ 
+
+
 
 let firstNumber;
 let secondNumber;
 let operator;
-
-
-function operate (a, b, o){
-    if(o == '+'){
-       return add(a, b);
-    }
-    else if(o == '-'){
-        return subtract(a, b);
-    }
-    else if(o == '*'){
-        return multiply(a, b);
-    }
-    else if(o == '/'){
-        return divide(a, b);
-    }
-}
 
 function add(a, b){
     return a + b;
